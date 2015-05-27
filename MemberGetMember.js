58af8884eb3b8e7aba0f8501b183198b040c825a
Gustavo Bodra <gustavobodra@gmail.com>
2015-05-26 23:55:45 -0300

    Processo de login adicionado na API

--- MemberGetMember.js
+++ MemberGetMember.js
@@ -8,85 +8,121 @@ var app = express();
 var client = new usergrid.client({
   orgName:'gustavobodra',
   appName:'membergetmember',
-  authType: usergrid.AUTH_CLIENT_ID,
-  clientId:'YXA6PaRAYAGuEeWpHH2IJkmohw',
-  clientSecret:'YXA6EJ6F8Dw1Q9oVpyg-vhw5VoUDG78',
+  authType: usergrid.AUTH_APP_USER
 });
 
 app.use(bodyParser.json());
 
+app.post("/logon", function(req, res){
+  client.login(req.body.Username, req.body.Password, function (err, response){
+    if (err){
+      res.send("Erro ao autenticar usuário");
+    } else {
+      res.send(client.token);
+    }
+  });
+});
+
 app.post("/invite", function (req, res){
+  if (check.unemptyString(client.token)){
+    if (check.unemptyString(req.body.Member.Id)
+    && check.unemptyString(req.body.Member.FullName)
+    && check.unemptyString(req.body.Member.Email)
+    && check.unemptyString(req.body.Friend.FullName)
+    && check.unemptyString(req.body.Friend.Email)
+    && check.unemptyString(req.body.Friend.PhoneMobile)
+    && check.unemptyString(req.body.Friend.CampaignCode)){
 
-  if (check.unemptyString(req.body.Member.Id)
-  && check.unemptyString(req.body.Member.Name)
-  && check.unemptyString(req.body.Member.Email)
-  && check.unemptyString(req.body.Friend.Name)
-  && check.unemptyString(req.body.Friend.Email)
-  && check.unemptyString(req.body.Friend.PhoneMobile)
-  && check.unemptyString(req.body.Friend.CampaignCode)){
+      var inputMember = {
+        type:'members',
+        Id:req.body.Member.Id,
+        FullName:req.body.Member.FullName,
+        Email:req.body.Member.Email
+      };
 
-    var inputMember = {
-      type:'members',
-      Id:req.body.Member.Id,
-      Name:req.body.Member.Name,
-      Email:req.body.Member.Email
-    };
+      var inputFriend = {
+        type:'friends',
+        FullName:req.body.Friend.FullName,
+        Email:req.body.Friend.Email,
+        PhoneMobile:req.body.Friend.PhoneMobile,
+        CampaignCode:req.body.Friend.CampaignCode
+      };
 
-    var inputFriend = {
-      type:'friends',
-      Name:req.body.Friend.Name,
-      Email:req.body.Friend.Email,
-      PhoneMobile:req.body.Friend.PhoneMobile,
-      CampaignCode:req.body.Friend.CampaignCode
-    };
+      async.waterfall([
+        function(callback) {
+          client.createEntity(inputMember, function (err, member){
+            if (err){
+              console.log("Error while creating member");
+            } else {
+              member.set('clientId','1');//DUMMY, MUDAR PARA O USUÁRIO DA SESSÃO
 
-    async.waterfall([
-      function(callback) {
-        client.createEntity(inputMember, function (err, member){
-          if (err){
-            console.log("Error while creating member");
-          } else {
-            member.set('clientId','1');//DUMMY, MUDAR PARA O USUÁRIO DA SESSÃO
+              member.save(function (err){
+                if (err){
+                  console.log("Error while saving member");
+                } else {
+                  callback(null, member);
+                }
+              });
+            }
+          });
+        },
+        function(member, callback) {
+          client.createEntity(inputFriend, function (err, friend){
+            if (err){
+              console.log("Error while creating friend");
+            } else {
+              friend.save(function (err){
+                if (err){
+                  console.log("Error while saving friend");
+                } else {
 
-            member.save(function (err){
-              if (err){
-                console.log("Error while saving member");
-              } else {
-                callback(null, member);
-              }
-            });
-          }
-        });
-      },
-      function(member, callback) {
-        client.createEntity(inputFriend, function (err, friend){
-          if (err){
-            console.log("Error while creating friend");
-          } else {
-            friend.save(function (err){
-              if (err){
-                console.log("Error while saving friend");
-              } else {
-                //TODO: conectar este invitee a uma campanha
-                member.connect('invites', friend, function (err, data){
-                  if (err){
-                    console.log("Error while connecting friend");
-                  } else {
+                  async.parallel([
+                    function (callback){
+                      var retrievalObj = {
+                        type: 'campaign',
+                        uuid:inputFriend.CampaignCode
+                      };
+
+                      client.getEntity(retrievalObj, function (err, campaign){
+                        if (err){
+                          console.log("Error while loading campaign object");
+                        } else {
+                          friend.connect('belongs', campaign, function (err, data) {
+                            if (err){
+                              console.log("Error while connecting friend to campaign");
+                            }
+                          });
+                        }
+                      });
+                    },
+                    function (callback){
+                      member.connect('invites', friend, function (err, data){
+                        if (err){
+                          console.log("Error while connecting friend");
+                        }
+                      });
+                    }
+                  ],
+                  function (err, results){
                     callback(null, 'done');
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
-    ], function (err, result) {
-      // result now equals 'done'
-    });
+                  });
+                }
+              });
+            }
+          });
+        }
+      ], function (err, result) {
+        console.log("Fim");
+      });
+      res.send("Fim");
+    } else {
+      res.status(400).send("Request does not match with the expected object");
+    }
   } else {
-    res.status(400).send("Request does not match with the expected object");
+    res.status(401).send("Please login to use the API");
   }
 });
+
 
 app.use("/", express.static(__dirname));
 
