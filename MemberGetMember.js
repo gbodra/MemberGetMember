cddd2ff796b55fac292aec612ab04dba08188d60
Gustavo Bodra <gustavobodra@gmail.com>
2015-05-27 23:36:02 -0300

    API finalizada para a POC. Iniciado o desenvolvimento da APP dummy para roadshow

--- MemberGetMember.js
+++ MemberGetMember.js
@@ -27,29 +27,29 @@ app.post("/invite", function (req, res){
   if (check.unemptyString(client.token)){
     if (check.unemptyString(req.body.Member.Id)
     && check.unemptyString(req.body.Member.FullName)
-    && check.unemptyString(req.body.Member.Email)
     && check.unemptyString(req.body.Friend.FullName)
-    && check.unemptyString(req.body.Friend.Email)
+    && check.unemptyString(req.body.Friend.Id)
     && check.unemptyString(req.body.Friend.PhoneMobile)
     && check.unemptyString(req.body.Friend.CampaignCode)){
 
       var inputMember = {
         type:'members',
-        Id:req.body.Member.Id,
         FullName:req.body.Member.FullName,
-        Email:req.body.Member.Email
+        name:req.body.Member.Id,
+        getOnExist:true
       };
 
       var inputFriend = {
         type:'friends',
         FullName:req.body.Friend.FullName,
-        Email:req.body.Friend.Email,
+        name:req.body.Friend.Id,
         PhoneMobile:req.body.Friend.PhoneMobile,
-        CampaignCode:req.body.Friend.CampaignCode
+        CampaignCode:req.body.Friend.CampaignCode,
+        getOnExist:true
       };
 
       async.waterfall([
-        function(callback) {
+        function(callback) { //SALVA O OBJETO MEMBER NA BASE
           client.createEntity(inputMember, function (err, member){
             if (err){
               console.log("Error while creating member");
@@ -66,7 +66,7 @@ app.post("/invite", function (req, res){
             }
           });
         },
-        function(member, callback) {
+        function(member, callback) { //SALVA O OBJETO FRIEND NA BASE
           client.createEntity(inputFriend, function (err, friend){
             if (err){
               console.log("Error while creating friend");
@@ -75,46 +75,71 @@ app.post("/invite", function (req, res){
                 if (err){
                   console.log("Error while saving friend");
                 } else {
+                  var memberFriend = {
+                    memberData:member,
+                    friendData:friend
+                  };
 
-                  async.parallel([
-                    function (callback){
-                      var retrievalObj = {
-                        type: 'campaign',
-                        uuid:inputFriend.CampaignCode
-                      };
-
-                      client.getEntity(retrievalObj, function (err, campaign){
-                        if (err){
-                          console.log("Error while loading campaign object");
-                        } else {
-                          friend.connect('belongs', campaign, function (err, data) {
-                            if (err){
-                              console.log("Error while connecting friend to campaign");
-                            }
-                          });
-                        }
-                      });
-                    },
-                    function (callback){
-                      member.connect('invites', friend, function (err, data){
-                        if (err){
-                          console.log("Error while connecting friend");
-                        }
-                      });
-                    }
-                  ],
-                  function (err, results){
-                    callback(null, 'done');
-                  });
+                  callback(null, memberFriend);
                 }
               });
             }
           });
         }
-      ], function (err, result) {
-        console.log("Fim");
+      ], function (err, result) { //FAZ OS RELACIONAMENTOS NECESSÁRIOS
+        var friend = result.friendData;
+        var member = result.memberData;
+
+        async.parallel([
+          function (callback){
+            var retrievalObj = {
+              type: 'campaign',
+              uuid:inputFriend.CampaignCode
+            };
+
+            client.getEntity(retrievalObj, function (err, campaign){ //RELACIONAMENTOS COM O OBJETO CAMPANHA
+              if (err){
+                console.log("Error while loading campaign object");
+              } else {
+                async.parallel([
+                  function (callback){
+                    friend.connect('belongs', campaign, function (err, data) {
+                      if (err){
+                        console.log("Error while connecting friend to campaign");
+                      }
+                    });
+                  },
+                  function (callback){
+                    member.connect('belongs', campaign, function (err, data) {
+                      if (err){
+                        console.log("Error while connecting member to campaign");
+                      }
+                    });
+                  }
+                ]);
+              }
+            });
+
+            callback(null,'one');
+          },
+          function (callback){ // RELACIONAMENTO ENTRE MEMBER E FRIEND
+            member.connect('invites', friend, function (err, data){
+              if (err){
+                console.log("Error while connecting friend");
+              }
+            });
+
+            callback(null, 'two');
+          }
+        ],
+        function (err, result){
+          var output = {
+            memberUuid:member.get('uuid'),
+            friendUuid:friend.get('uuid')
+          };
+          res.send(output);
+        });
       });
-      res.send("Fim");
     } else {
       res.status(400).send("Request does not match with the expected object");
     }
@@ -123,6 +148,41 @@ app.post("/invite", function (req, res){
   }
 });
 
+app.post("/discount", function (req, res){
+  if (check.unemptyString(client.token)){
+    if (check.unemptyString(req.body.type)
+    && check.unemptyString(req.body.uuid)){
+
+      var query = {
+        type:req.body.type,
+        uuid:req.body.uuid
+      };
+
+      client.getEntity(query, function(err, entity){
+        if (err){
+          res.send("Erro1");
+        } else {
+          entity.getConnections('belongs', function (error, connections) {
+            if (error) {
+              res.send("Erro2");
+            } else {
+              /*var output = {
+
+              };
+              for (connection in connections.entities){
+                console.log(connections.entities[connection].name);
+              }*/
+              res.send(connections.entities);
+            }
+          });
+        }
+      });
+
+    }
+  } else {
+    res.status(401).send("Please login to use the API");
+  }
+});
 
 app.use("/", express.static(__dirname));
 
